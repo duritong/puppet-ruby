82a12afdaf0fb82ab3f380895de6c5bcf864fa8a
mh <mh@immerda.ch>
2023-02-21 22:40:00 +0100

    install newer version on EL9

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,6 +1,11 @@
 class ruby::mysql {
   include ruby
-  package{'ruby-mysql':
+  if versioncmp($facts['os']['release']['major'],'9') < 0 {
+    $pkg_name = 'ruby-mysql'
+  } else {
+    $pkg_name = 'ruby-mysql2'
+  }
+  package{$pkg_name:
     ensure => present,
     require => Package['ruby'],
   }
